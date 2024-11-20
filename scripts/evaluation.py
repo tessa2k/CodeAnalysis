8baab8931785d70a76f1b952b5be8e2605807f7d
Xincheng Cai <xincheng.cai@duke.edu>
2024-11-20 15:37:02 -0500

    update scripts

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,6 +1,9 @@
 '''
 evaluate gpt generated metadata types
 '''
+import json
+from pprint import pprint
+
 class Evaluate:
     def __init__(self, y_true, y_pred):
         """
@@ -51,11 +54,13 @@ class Evaluate:
             "precision": self.precision(),
             "recall": self.recall()
         }
+    
 
-if __name__ == "__main__":
+def test_examples():
     # case I: normal predictions
     y_pred = ['I Calcium', 'I h', 'I_CAP', 'I_CLAMP']
     y_true = ['I L high threshold', 'I h', 'I K,Ca', 'I Calcium', 'I_HERG']
+    print(f"Predict - {y_pred}; \nTrue - {y_true}")
     eval_result = Evaluate(y_pred, y_true).result()
     print(eval_result)
     print()
@@ -63,6 +68,7 @@ if __name__ == "__main__":
     # case II: ground truth is empty
     y_pred = ['I Ca', 'I h', 'I_CAB', 'I_CAP', 'I_CLAMP']
     y_true = None
+    print(f"Predict - {y_pred}; \nTrue - {y_true}")
     eval_result = Evaluate(y_pred, y_true).result()
     print(eval_result)
     print()
@@ -70,6 +76,51 @@ if __name__ == "__main__":
     # case III: prediction is empty
     y_pred = []
     y_true = ['I L high threshold', 'I h', 'I K,Ca', 'I Calcium', 'I_HERG']
+    print(f"Predict - {y_pred};\nTrue - {y_true}")
     eval_result = Evaluate(y_pred, y_true).result()
     print(eval_result)
     print()
+
+if __name__ == "__main__":
+    # test_examples()
+    sample_list = [114665, 118434, 114424, 105383, 113949]
+    metadata_file_path = '/Users/cynthia/Desktop/Capstone-CodeAnalysis/Data/model_metadata.json'
+    with open(metadata_file_path, 'r', encoding='utf-8') as json_file:
+        metadata = json.load(json_file)
+    test_file_path = '/Users/cynthia/Desktop/Capstone-CodeAnalysis/Data/prompt_test.json'
+    with open(test_file_path, 'r', encoding='utf-8') as json_file:
+        output = json.load(json_file)
+    
+    most_relevant, potentially_relevant = output[0]["results"], output[1]["results"]
+    result_1, result_2 = {}, {}
+    for i, num in enumerate(sample_list):
+        if num == 118434: continue
+        y_true = metadata[str(num)]["currents"]
+
+        y_pred = most_relevant[i]["metadata"].split(',')
+        y_pred = [i.strip() for i in y_pred]
+        result_1[num] = Evaluate(y_pred, y_true).result()
+        result_1[num]["True"] = y_true
+        result_1[num]["Predict"] = y_pred
+
+        y_pred = potentially_relevant[i]["metadata"].split(',')
+        y_pred = [i.strip() for i in y_pred]
+        result_2[num] = Evaluate(y_pred, y_true).result()
+        result_2[num]["True"] = y_true
+        result_2[num]["Predict"] = y_pred
+
+    print("Most relevant:")
+    pprint(result_1)
+    print()
+    print("Potentially relevant:")
+    pprint(result_2)
+
+    output_file_path = '/Users/cynthia/Desktop/Capstone-CodeAnalysis/Data/sample_performance.json'
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        json.dump({
+            "Most Relevant": result_1,
+            "Potentially Relevant": result_2
+        }, output_file, indent=4)
+
+    
+    
