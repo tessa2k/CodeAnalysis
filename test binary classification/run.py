fe43913ecb5abad8845446ee48954a3bac262086
Cyngua <cynthia-cai@outlook.com>
2025-01-21 12:02:55 -0500

    run binary classification

--- test binary classification/run.py
+++ test binary classification/run.py
@@ -19,8 +19,8 @@ def run(para):
     results = {}
     client = OpenAI(api_key=para["api_key"], organization=para["organization"])
     topic = para["topic"] 
-    for entry in os.listdir(para["directory"]):
-        full_path = os.path.join(para["directory"], entry)
+    for entry in tqdm(os.listdir(para["input_dir"])):
+        full_path = os.path.join(para["input_dir"], entry)
         code = entry.split('_')[0]
         with open(full_path, 'r', encoding='utf-8') as file:
             file_content = file.read()
@@ -32,20 +32,23 @@ def run(para):
             f"If the answer is yes, please answer Y; otherwise, answer N. DO NOT analyze."
         )
         chat_completion = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[{"role": "user", "content": prompt}],
+            model="gpt-4o",
+            messages=[{"role": "user", "content": prompt}],
         )
 
         response_dict = chat_completion.to_dict()
-        print(code)
-        print(response_dict)
-        break
+        results[code] = response_dict["choices"][0]["message"]["content"]
+
+    with open(para["output_dir"], 'w', encoding='utf-8') as json_file:
+        json.dump(results, json_file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
+    load_dotenv()
     para = {
         "api_key": os.getenv('API_KEY'),
         "organization": os.getenv('ORGANIZATION'),
         "topic": "Parkinson's",
-        "directory": "../sampleParkinsons/match_file" 
-    } 
+        "input_dir": "../sampleParkinsons/match_file",
+        "output_dir": "binary_classification_test.json"
+    }
     run(para)
