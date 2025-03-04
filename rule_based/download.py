6169177de424e11fc87f68d4b7ef103fb1380b6e
Cyngua <cynthia-cai@outlook.com>
2025-03-03 22:24:36 -0500

    download 2022 data

--- rule_based/download.py
+++ rule_based/download.py
@@ -20,9 +20,9 @@ def delete_zip_files(directory):
                     print(f"Error deleting {file_path}: {e}")
 
 def main():
-    file_code_list = filter_models_by_year() # min_year = 2020 by default
-    print("Total number of models after 2020:", len(file_code_list))
-    sample_folder = '../2020_Data'
+    file_code_list = filter_models_by_year(2022) # min_year = 2020 by default
+    print("Total number of models after 2022:", len(file_code_list))
+    sample_folder = '../2022_Data'
     num_file = len(file_code_list)
     download_and_unzip_files(file_code_list, sample_folder, num_file)
     delete_zip_files(sample_folder)
